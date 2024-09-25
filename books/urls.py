5577a3a7659d6623cb382211deac37198e55064f
demzyAkpan <demzyukonta@gmail.com>
2024-09-25 11:50:45 +0100

    api built

---
+++ books/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import BookListView
+
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', BookListView.as_view(), name='home'),
+]
